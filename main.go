acaa414cd2c004e3bc4e4d803fcbcf6542af2364
Temperskiy <nikitaabrosimov495@gmail.com>
2026-06-24 03:32:11 +0500

    main.go

--- main.go
+++ main.go
@@ -7,18 +7,13 @@ import (
 )
 
 func main() {
-	// Создаем пустой файл ключа, чтобы Dendrite его увидел при запуске
-	if _, err := os.Stat("matrix_key.pem"); os.IsNotExist(err) {
-		log.Println("Creating matrix_key.pem...")
-		os.WriteFile("matrix_key.pem", []byte(""), 0644)
-	}
-
-	log.Println("Starting Dendrite...")
-	// Запускаем Dendrite с портом 10000
-	cmdRun := exec.Command("/opt/render/project/go/bin/dendrite", "--config", "dendrite.yaml", "-http-bind-address", ":10000")
-	cmdRun.Stdout = os.Stdout
-	cmdRun.Stderr = os.Stderr
-	if err := cmdRun.Run(); err != nil {
-		log.Fatalf("Dendrite crashed: %v", err)
-	}
+	// Создаем ключ, если нет
+	os.WriteFile("matrix_key.pem", []byte("SIGNING_KEY_MUST_BE_32_BYTES_LONG_!!!"), 0644)
+	
+	log.Println("Launching Dendrite directly...")
+	// Запускаем напрямую
+	cmd := exec.Command("/opt/render/project/go/bin/dendrite", "--config", "dendrite.yaml", "-http-bind-address", ":10000")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
